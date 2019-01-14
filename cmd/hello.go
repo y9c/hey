7d418ac11050014e7286878c06b7cca192413520
yechang <yech1990@gmail.com>
2019-01-15 02:14:46 +0800

    add hello command

--- cmd/hello.go
+++ cmd/hello.go
@@ -3,26 +3,127 @@ package cmd
 import (
 	"fmt"
 	"os/user"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/spf13/cobra"
 )
 
-// testCmd represents the test command
-var testCmd = &cobra.Command{
+var helloCmd = &cobra.Command{
 	Use:   "hello",
 	Short: "A testing command",
 	Long:  `Just for testing`,
 	Run: func(cmd *cobra.Command, args []string) {
 		user, err := user.Current()
 		if err != nil {
-			fmt.Println("Hello")
+			sayHello("Hello!")
 		} else {
-			fmt.Println("Hi, " + user.Username)
+			sayHello("Hi, " + user.Username + "!")
 		}
 
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(testCmd)
+	rootCmd.AddCommand(helloCmd)
+}
+
+// buildBalloon takes a slice of strings of max width maxwidth
+// prepends/appends margins on first and last line, and at start/end of each line
+// and returns a string with the contents of the balloon
+func buildBalloon(lines []string, maxwidth int) string {
+	var borders []string
+	count := len(lines)
+	var ret []string
+
+	borders = []string{"/", "\\", "\\", "/", "|", "<", ">"}
+
+	top := " " + strings.Repeat("_", maxwidth+2)
+	bottom := " " + strings.Repeat("-", maxwidth+2)
+
+	ret = append(ret, top)
+	if count == 1 {
+		s := fmt.Sprintf("%s %s %s", borders[5], lines[0], borders[6])
+		ret = append(ret, s)
+	} else {
+		s := fmt.Sprintf(`%s %s %s`, borders[0], lines[0], borders[1])
+		ret = append(ret, s)
+		i := 1
+		for ; i < count-1; i++ {
+			s = fmt.Sprintf(`%s %s %s`, borders[4], lines[i], borders[4])
+			ret = append(ret, s)
+		}
+		s = fmt.Sprintf(`%s %s %s`, borders[2], lines[i], borders[3])
+		ret = append(ret, s)
+	}
+
+	ret = append(ret, bottom)
+	return strings.Join(ret, "\n")
+}
+
+// tabsToSpaces converts all tabs found in the strings
+// found in the `lines` slice to 4 spaces, to prevent misalignments in
+// counting the runes
+func tabsToSpaces(lines []string) []string {
+	var ret []string
+	for _, l := range lines {
+		l = strings.Replace(l, "\t", "    ", -1)
+		ret = append(ret, l)
+	}
+	return ret
+}
+
+// calculatemaxwidth given a slice of strings returns the lenght of the
+// string with max length
+func calculateMaxWidth(lines []string) int {
+	w := 0
+	for _, l := range lines {
+		len := utf8.RuneCountInString(l)
+		if len > w {
+			w = len
+		}
+	}
+
+	return w
+}
+
+// normalizeStringsLength takes a slice of strings and appends
+// to each one a number of spaces needed to have them all the same number
+// of runes
+func normalizeStringsLength(lines []string, maxwidth int) []string {
+	var ret []string
+	for _, l := range lines {
+		s := l + strings.Repeat(" ", maxwidth-utf8.RuneCountInString(l))
+		ret = append(ret, s)
+	}
+	return ret
+}
+
+func sayHello(content string) {
+	lines := []string{content}
+
+	var stegosaurus = `         \                      .       .
+          \                    / ` + "`" + `.   .' "
+           \           .---.  <    > <    >  .---.
+            \          |    \  \ - ~ ~ - /  /    |
+          _____           ..-~             ~-..-~
+         |     |   \~~~\\.'                    ` + "`" + `./~~~/
+        ---------   \__/                         \__/
+       .'  O    \     /               /       \  "
+      (_____,    ` + "`" + `._.'               |         }  \/~~~/
+       ` + "`" + `----.          /       }     |        /    \__/
+             ` + "`" + `-.      |       /      |       /      ` + "`" + `. ,~~|
+                 ~-.__|      /_ - ~ ^|      /- _      ` + "`" + `..-'
+                      |     /        |     /     ~-.     ` + "`" + `-. _  _  _
+                      |_____|        |_____|         ~ - . _ _ _ _ _>
+
+	`
+
+	lines = tabsToSpaces(lines)
+	maxwidth := calculateMaxWidth(lines)
+	messages := normalizeStringsLength(lines, maxwidth)
+	balloon := buildBalloon(messages, maxwidth)
+	fmt.Println(balloon)
+	fmt.Println(stegosaurus)
+	fmt.Println()
 }
